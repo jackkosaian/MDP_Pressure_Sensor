bd633f691c7afb655b39ed874de735ef7e7af1fb
Jack Kosaian <jackkosaian@gmail.com>
2014-04-11 22:03:34 -0400

    Added yellow LED and altered general format

--- ArduinoPressureReading/ArduinoPressureReading.ino
+++ ArduinoPressureReading/ArduinoPressureReading.ino
@@ -1,5 +1,6 @@
-int ledPin = 4; // LED pin for pressure warning
-int initializationLED = 2; // LED pin for initialization
+int redLED = 4; // LED pin for pressure warning
+int greenLED = 2; // LED pin for start use
+int yellowLED = 3; // LED pin for initialization
 int buzzerPin = 7; // Buzzer pin
 int pressurePin = A0; // Pin to take pressure reading
 int pressureValue; // Pressure value in analog units
@@ -20,12 +21,15 @@ float average;
 int averageCounter = 0;
 boolean turnOnLED = false;
 long numCycles = 2500 * 2 / 1000;
+int canBuzz = 0;
 
 void setup(){
  
- pinMode(initializationLED, OUTPUT); 
+ pinMode(yellowLED, OUTPUT); 
   
- pinMode(ledPin, OUTPUT);
+ pinMode(redLED, OUTPUT);
+ 
+ pinMode(greenLED, OUTPUT);
  
  pinMode(buzzerPin, OUTPUT);
  
@@ -38,74 +42,89 @@ void setup(){
 }
 
 void loop(){
-  
- pressureValue = analogRead(pressurePin);
- pressureInmmHg = (((float)pressureValue * 5 / 1024) - .5) * 15 / 4 * 51.7149326; 
  
- // Turn on LED if at desiredVoltage +/- 5
- if (pressureValue > desiredVoltage - 5 && pressureValue < desiredVoltage + 5)
- {
-     // Set variable to allow LED lighting
-     if( averageCounter == 0){
-       averageCounter = 1;
+  if (canBuzz == 1){
+     for(long i = 0; i < 1250; i++)
+     {
+      digitalWrite(buzzerPin,HIGH); // write the buzzer pin high to push out the diaphram
+      delayMicroseconds(delayValue); // wait for the calculated delay value
+      digitalWrite(buzzerPin,LOW); // write the buzzer pin low to pull back the diaphram
+      delayMicroseconds(delayValue); // wait againf or the calculated delay value
      }
+   }
+  
+  else {
+     pressureValue = analogRead(pressurePin);
+     pressureInmmHg = (((float)pressureValue * 5 / 1024) - .5) * 15 / 4 * 51.7149326; 
+ 
+     // Turn on LED if at desiredVoltage +/- 5
+     if (pressureValue > desiredVoltage - 5 && pressureValue < desiredVoltage + 5)
+     {
+         // Set variable to allow LED lighting
+         if( averageCounter == 0){
+           averageCounter = 1;
+         }
      
-     // Turn on green LED when user has reached desiredVoltage
-     if (averageCounter >= 0 && averageCounter <= 10){
-       turnOnLED = true;
+         // Turn on green LED when user has reached desiredVoltage
+         if (averageCounter >= 0 && averageCounter <= 10){
+           turnOnLED = true;
+         }
+         else {
+           turnOnLED = false;
+         }
+     
      }
+ 
      else {
        turnOnLED = false;
      }
- }
  
- else {
-   turnOnLED = false;
- }
- 
- if (turnOnLED == true)
- {
-  digitalWrite(initializationLED, HIGH); 
- }
- else {
-  digitalWrite(initializationLED, LOW);
- }
+     if (turnOnLED == true)
+     {
+      digitalWrite(yellowLED, HIGH); 
+     }
+     else {
+      digitalWrite(yellowLED, LOW);
+     }
 
-if (averageCounter >= 1 && averageCounter <= 10)
-{
-  //total -= readings[index];
-  //readings[index] = pressureValue;
-  total += pressureValue;
-  index += 1; // increment the index
-  average = (float)total / (float)numReadings; 
+    if (averageCounter >= 1 && averageCounter <= 10)
+    {
+      total += pressureValue;
+      index += 1; // increment the index
+      average = (float)total / (float)numReadings; 
  
-  if (averageCounter == 10){
-     basePressure = average;
+      if (averageCounter == 10){
+         basePressure = average;
+      } 
+      averageCounter += 1;
+    }
+
+    if (averageCounter >= 10) {
+      digitalWrite(greenLED, HIGH);
+    }
+    else{
+      digitalWrite(greenLED, LOW);
+    }
+
+     Serial.print("mmHg: ");
+     Serial.print(pressureInmmHg);
+     Serial.print("          Analog Value: ");
+     Serial.print(pressureValue);
+     Serial.print("          Average: ");
+     Serial.print(average);
+     Serial.print("          Base Pressure: ");
+     Serial.print(basePressure);
+     Serial.print("\n");
+ 
+     // If pressure gets too low
+     if(pressureValue < basePressure - 20){
+      digitalWrite(redLED, HIGH);
+      canBuzz = 1;
+     }
+ 
+     else {
+      digitalWrite(redLED, LOW);
+      digitalWrite(buzzerPin, LOW); 
+     }
   } 
-  averageCounter += 1;
-}
-
- Serial.print("mmHg: ");
- Serial.print(pressureInmmHg);
- Serial.print("          Analog Value: ");
- Serial.print(pressureValue);
- Serial.print("          Average: ");
- Serial.print(average);
- Serial.print("          Base Pressure: ");
- Serial.print(basePressure);
- Serial.print("\n");
- 
- // If pressure gets too low
- if(pressureValue < basePressure - 20){
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(buzzerPin,HIGH); // write the buzzer pin high to push out the diaphram
-  delayMicroseconds(delayValue); // wait for the calculated delay value
-  digitalWrite(buzzerPin,LOW); // write the buzzer pin low to pull back the diaphram
-  delayMicroseconds(delayValue); // wait againf or the calculated delay value
- }
- 
- else {
-  digitalWrite(ledPin, LOW);
-  digitalWrite(buzzerPin, LOW); 
- } 
 }
